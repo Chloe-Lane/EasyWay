401ab8ebcc56e38141b1d054ef4ae1ee2103f209
Justin Rieko Tolentino <jstolentino2@student.hau.edu.ph>
2025-02-11 19:59:21 +0800

    may binago

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,6 +2,8 @@ from django.http import JsonResponse
 from base.rooms import rooms
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Room
+from .serializers import RoomSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -20,14 +22,13 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getRooms(request):
-    return Response(rooms)
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def getRoom(request, pk):
-    room = None
-    for i in rooms:
-        if i['_id'] == pk:
-            room = i
-            break
-    return Response(room)
+    room = Room.objects.get(_id=pk)
+    serializer = RoomSerializer(room, many=False)
+    return Response(serializer.data)
